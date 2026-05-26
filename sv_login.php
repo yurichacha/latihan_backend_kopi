fe84432569b66692129fdc1534855e1402051d72
yurichacha <esteryuricha@gmail.com>
2026-05-26 09:23:15 +0700

    update sv_login.php

--- sv_login.php
+++ sv_login.php
@@ -8,10 +8,10 @@ $password = $_POST['password'];
 
 $sql = "select * from users where username='$username' and password='$password'";
 $query = mysqli_query($conn,$sql);
-$num = mysqli_num_check($query);
+$num = mysqli_num_rows($query);
 
 if($num > 0){
-    header("Location: dashboard.php");
+    header("Location: admin/dashboard.php");
     exit;
 }else{
     header("Location: login.php");
